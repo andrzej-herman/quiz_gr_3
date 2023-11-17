76ee9f12831f0bcadf889edbcb6997a89b865f12
andrzej-herman <aherman@san.edu.pl>
2023-11-17 12:44:37 +0100

    Add project

---
+++ QuizApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using QuizApp.backend;
+
+Console.WriteLine("Hello, World!");
+
+int x;
+Console.WriteLine(x);
+
+int f = 3;
+bool d = true;
+Question pytanie1 = new Question();
+pytanie1.Category = 100;
+pytanie1.Content = "Jaka jes ???";
+
+
+Console.WriteLine(pytanie1);
+
+for (int i = 0; i < f; i++)
+{
+
+}
+
+Console.ReadLine();
---
+++ QuizApp/backend/Question.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizApp.backend
+{
+    public class Question
+    {
+        public int Category { get; set; }
+        public string Content { get; set; }
+    }
+}
